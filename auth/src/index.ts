0eceb78e350e225bf6d64a4862071480dd14791b
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-05-20 23:33:12 -0500

    New enn-point to test ingress srv

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
-app.get('/',  (req, res) => {
+app.get('/api/users/currentuser',  (req, res) => {
 	res.send({ok:true, status: 200})
 })
 
 app.listen(PORT, () => {
-	console.log('v2')
+	console.log('v9')
 	console.log(`Auth services is running on port ${PORT}`)
 })
