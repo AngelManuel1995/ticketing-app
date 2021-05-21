8ed3cdff6e2cab11309299f2def7348d29cef578
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-05-20 20:58:57 -0500

    Namespace to k8s cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+app.get('/',  (req, res) => {
+	res.send({ok:true, status: 200})
+})
+
 app.listen(PORT, () => {
 	console.log(`Auth services is running on port ${PORT}`)
 })
