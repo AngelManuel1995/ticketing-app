b803b3c84bdfd644e9c401a28cfc56c46fae986b
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-07-08 21:02:08 -0500

    Creating a user model and user interface

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,16 +1,31 @@
 import mongoose from 'mongoose'
 
+interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+	build(attrs: UserAttrs): any
+}
+
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
 })
 
-const User = mongoose.model('User', UserSchema)
+UserSchema.statics.build = async function (attrs: UserAttrs) {
+	return new User(attrs)
+}
+
+const User = mongoose.model<any, UserModel>('User', UserSchema)
+
+User.build({email:'as', password:'as'})
 
 export { User }
