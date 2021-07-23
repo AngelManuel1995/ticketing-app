357144ec24697b22350f99758ce7f45b4dd2b22d
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-07-22 20:47:32 -0500

    Getting current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,20 @@
 'use strict'
 
+import jwt from 'jsonwebtoken'
 import { Router } from 'express'
 
 const api = Router()
 
 api.get('/api/users/currentuser',  (req, res) => {
-	res.send({ok:true, status: 200})
+	if(!req.session?.jwt) {
+		return res.send({currentUser: null})
+	}
+	try {
+		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+		res.send({currentUser: payload})
+	} catch (error) {
+		res.send({currentUser: null})
+	}
 })
 
 export { api as currentUserRouter }
