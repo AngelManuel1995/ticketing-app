1576f517d52805f77b797badf2128c438b613828
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-07-22 19:54:13 -0500

    Validationg sign in done

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,16 @@
 'use strict'
 
-import { Router } from 'express'
-
+import { Router, Request, Response } from 'express'
+import { validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { SIGNIN_VALIDATORS } from '../validators/validators'
 const api = Router()
 
-api.post('/api/users/signin',  (req, res) => {
+api.post('/api/users/signin', SIGNIN_VALIDATORS, async (req: Request, res: Response) => {
+	const errors = validationResult(req)
+	if(!errors.isEmpty()){
+		throw new RequestValidationError(errors.array())
+	}
 	res.send({ok:true, status: 200})
 })
 
--- auth/src/validators/validators.ts
+++ auth/src/validators/validators.ts
@@ -10,3 +10,14 @@ export const SIGNUP_VALIDATORS = [
 		.isLength({min: 4, max: 20})
 		.withMessage('Password must be between 4 and 20 characters')
 ]
+
+export const SIGNIN_VALIDATORS = [
+	body('email')
+		.trim()
+		.isEmail()
+		.withMessage('Email must be valid'),
+	body('password')
+		.trim()
+		.notEmpty()
+		.withMessage('You must supply a password')	
+]
