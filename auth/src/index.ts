01ee6594d841ff7fba5141962858793556e3a3fd
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-07-27 22:02:20 -0500

    refactoring code to make tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,34 +1,8 @@
 'use strict'
 
-import express from 'express'
-import mongoose  from 'mongoose'
-
-import 'express-async-errors'
-import cookieSession from 'cookie-session'
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signupRouter } from './routes/signup'
-import { signoutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-hanlder'
-import { NotFoundError } from './errors/not-found-error'
 const PORT = process.env.PORT || 3000
-const app = express()
-app.set('trust proxy', true)
-app.use(express.json())
-app.use(cookieSession({
-	signed: false,
-	secure: true
-}))
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
-
-app.all('*', async (req, res) => {
-	throw new NotFoundError()
-})
-app.use(errorHandler)
+import mongoose  from 'mongoose'
+import { app } from './app'
 
 const start = async () => {
 	if(!process.env.JWT_KEY){
