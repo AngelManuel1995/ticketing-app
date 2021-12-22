bd9a26fe685baaf85cb412ba7bbbe58c907d5ffe
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2021-12-21 22:11:16 -0500

    Clients creation with a radom id

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,20 @@
-import nats from 'node-nats-streaming'
+import nats, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
 
 console.clear()
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 	url: 'http://localhost:4222'
 })
 
 stan.on('connect', () => {
 	console.log('Listener connected to NATS')
 	const subscription = stan.subscribe('ticket:created')
-	subscription.on('message', (msg) => {
-		console.log('Message received', msg)
+	subscription.on('message', (msg: Message) => {
+		const data = msg.getData()
+		console.log('Message received', data, typeof data)
+		if(typeof data === 'string') {
+			console.log(`Received event #${msg.getSequence()}, with data: ${JSON.parse(data)['price']}`)
+		}
 	})
 })
