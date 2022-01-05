c4223cc0c50b4315266277aef08f45f9a0a6d79a
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2022-01-04 21:07:47 -0500

    Tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import 'express-async-errors'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError } from '@magtickets/common'
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 const app = express()
 app.set('trust proxy', true)
 app.use(express.json())
@@ -13,6 +14,7 @@ app.use(cookieSession({
 	secure: process.env.NODE_ENV !== 'test'
 }))
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 app.all('*', async (req, res) => {
 	throw new NotFoundError()
 })
--- tickets/src/validators/validators.ts
+++ tickets/src/validators/validators.ts
@@ -4,8 +4,7 @@ export const TICKET_CREATION = [
     body('title')
         .trim()
         .notEmpty()
-        .withMessage('Title must be provided')
-    ,
+        .withMessage('Title must be provided'),
     body('price')
         .isFloat({gt: 0})
         .withMessage('Price must be greater than 0')
