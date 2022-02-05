035de5f260a939e0cd981ed058ff672546bcc6ca
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2022-02-05 18:00:18 -0500

    Creating a NATS class with singleton pattern

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,12 +3,13 @@
 const PORT = process.env.PORT || 3000
 import mongoose  from 'mongoose'
 import { app } from './app'
-
+import { natsWrapper } from './nats-warraper'
 const start = async () => {
 	if(!process.env.JWT_KEY){
 		throw new Error('JWT_KEY must be defined')
 	}
 	try {
+		await natsWrapper.connect('ticketing', 'ajhasbjahsbhs', 'http://localhost:4222')
 		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',  {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,19 +3,26 @@
 import { requireAuth, validateRequest } from '@magtickets/common'
 import express, { Request, Response } from 'express'
 import { TICKET_CREATION } from '../validators/validators'
-import {  Ticket } from '../models/ticket'
+import { Ticket } from '../models/ticket'
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-warraper'
 const router = express.Router()
 
 router.post('/api/tickets', requireAuth, TICKET_CREATION, validateRequest, async (req: Request, res: Response) => {
-
-    const { price, title } = req.body
-    const ticket = Ticket.build({
-        price,
-        title,
-        userId: req.currentUser!.id
-    })
-    await ticket.save()
-    res.status(201).send({})
+	const { price, title } = req.body
+	const ticket = Ticket.build({
+		price,
+		title,
+		userId: req.currentUser!.id
+	})
+	await ticket.save()
+	new TicketCreatedPublisher(natsWrapper.client).publish({
+		id: ticket.id,
+		price: ticket.price,
+		title: ticket.title,
+		userId: ticket.userId
+	})
+	res.status(201).send({})
 })
 
 export {
