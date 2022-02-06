117826067476b57eaaeedae9ca516765a7941cff
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2022-02-05 22:00:15 -0500

    New node dockerfile version

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,19 +9,18 @@ const start = async () => {
 		throw new Error('JWT_KEY must be defined')
 	}
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',  {
+		await mongoose.connect('mongodb://auth:password@tickets-mongo-srv:27017/auth',  {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true
 		})
 		console.log('Connected to mongoDb')
+		app.listen(PORT, () => {
+			console.log(`Auth service is running on port ${PORT}`)
+		})
 	} catch (error) {
 		console.log(error)
 	}
-	app.listen(PORT, () => {
-		console.log('v9')
-		console.log(`Auth service is running on port ${PORT}`)
-	})
 }
 
 start()
