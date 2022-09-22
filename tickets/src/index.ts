1b552d0bd39d30f6eade9e2c4d8f743fdbaefc2b
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2022-09-22 14:35:31 -0500

    uploading the common module

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,13 +3,25 @@
 const PORT = process.env.PORT || 3000
 import mongoose  from 'mongoose'
 import { app } from './app'
-import { natsWrapper } from './nats-warraper'
+import { natsWrapper } from './nats-wrapper'
 const start = async () => {
 	if(!process.env.JWT_KEY){
 		throw new Error('JWT_KEY must be defined')
 	}
+	if(!process.env.NATS_CLIENT_ID){
+		throw new Error('NATS_CLIENT_ID must be defined')
+	}
+	if(!process.env.NATS_URL){
+		throw new Error('NATS_URL must be defined')
+	}
+	if(!process.env.NATS_CLUSTER_ID){
+		throw new Error('NATS_CLUSTER_ID must be defined')
+	}
+	if(!process.env.MONGO_URI){
+		throw new Error('MONGO_URI must be defined')
+	}
 	try {
-		await natsWrapper.connect('ticketing', 'ajhasbjahsbhs', 'http://nats-srv:4222')
+		await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
 		natsWrapper.client.on('close', () => {
 			console.log('NATS connection close')
 			process.exit()
@@ -17,7 +29,7 @@ const start = async () => {
 		process.on('SIGINT', () => natsWrapper.client.close())
 		process.on('SIGTERM', () => natsWrapper.client.close())
 
-		await mongoose.connect('mongodb://auth:password@tickets-mongo-srv:27017/auth',  {
+		await mongoose.connect(process.env.MONGO_URI,  {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -5,7 +5,7 @@ import express, { Request, Response } from 'express'
 import { TICKET_CREATION } from '../validators/validators'
 import { Ticket } from '../models/ticket'
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
-import { natsWrapper } from '../nats-warraper'
+import { natsWrapper } from '../nats-wrapper'
 const router = express.Router()
 
 router.post('/api/tickets', requireAuth, TICKET_CREATION, validateRequest, async (req: Request, res: Response) => {
